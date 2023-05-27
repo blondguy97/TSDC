28ccb925b4395fdd649cc660e2bc0d7def16c850
Jacob <creepstory@gmail.com>
2023-05-27 18:42:23 +0300

    end of desktop version

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// document.addEventListener('DOMContentLoaded', function () {
+
+
+
+//     // Hamburger
+
+//     const menu = document.querySelector('.header-list_mobile'),
+//         menuItem = document.querySelectorAll('.header-list__item'),
+//         hamburger = document.querySelector('.hamburger');
+
+//     hamburger.addEventListener('click', () => {
+//         hamburger.classList.toggle('hamburger_active');
+//         menu.classList.toggle('header-list_mobile_active');
+//     });
+
+//     menuItem.forEach(item => {
+//         item.addEventListener('click', () => {
+//             hamburger.classList.toggle('hamburger_active');
+//             menu.classList.toggle('header-list_mobile_active');
+//         })
+//     })
+
+//     // Modal
+
+
+
+
+// });
+
