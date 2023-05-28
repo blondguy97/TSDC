d5bf86b7880c9896b03b330f8504bfba06c7a46f
Jacob <creepstory@gmail.com>
2023-05-28 18:06:09 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-    const menu = document.querySelector('.menu'),
+    const menu = document.querySelector('.menu-list'),
         hamburger = document.querySelector('.hamburger');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+        menu.classList.toggle('menu-list_active');
     });
 
  
